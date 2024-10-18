9b6514682c97e8390a5ba3a90846854e2fbf1b83
Elmer Balbin <contact@elmer.dev>
2024-10-18 14:25:02 -0700

    updated ex05

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -1,5 +1,5 @@
 // Create a function called mixQueue that rearranges the queue so that the first half is interleaved/mixed with the second half
-// For example: 1 2 3 4 5 6 becomes 1 4 2 5 4 6
+// For example: 1 2 3 4 5 6 becomes 1 4 2 5 3 6
 // Make sure to implement the Queue principle (FIFO)
 
 const Queue = require('../lib/Queue');
@@ -17,4 +17,4 @@ queue.enqueue(5);
 queue.enqueue(6);
 
 mixQueue(queue);
-console.log(queue.printQueue()); // Output: 1 4 2 5 4 6
+console.log(queue.printQueue()); // Output: 1 4 2 5 3 6
